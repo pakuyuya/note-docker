ca201778d51bd3a13f5dbf992735d06a6ed9041e
pakuyuya <pakuyuya@yahoo.co.jp>
2019-08-23 23:42:49 +0900

    batch elasticsearch 化してみるが動かない

--- 03.recipts/compose-batch-with-fulentd/docker-batch-fulentd/batch/src/main.go
+++ 03.recipts/compose-batch-with-fulentd/docker-batch-fulentd/batch/src/main.go
@@ -4,10 +4,10 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"os"
 	"io/ioutil"
+	"os"
 	"time"
-	
+
 	_ "github.com/lib/pq"
 	"github.com/urfave/cli"
 )
@@ -18,30 +18,27 @@ func main() {
 
 func run(args []string) error {
 	app := cli.NewApp()
-	
+
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name: "SqlFile",
+			Name:  "SqlFile",
 			Value: "",
 		},
 	}
 	app.Action = func(c *cli.Context) error {
 		InfoLog("Batch started")
 
-		// 引数取得
 		sqlfile := c.String("SqlFile")
 		if sqlfile == "" {
 			return errors.New("Parameter 'SqlFile' is required.")
 		}
-		
-		// DB接続
+
 		db, err := sql.Open("postgres", GetConnectionString())
 		if err != nil {
 			return err
 		}
-		defer db.Close() // defer: returnなど関数が終わるときに実行
-		
-		// SQLファイル読み取り
+		defer db.Close()
+
 		file, err := os.Open(sqlfile)
 		if err != nil {
 			return err
@@ -49,20 +46,19 @@ func run(args []string) error {
 		defer file.Close()
 		bytes, _ := ioutil.ReadAll(file)
 		query := string(bytes)
-		
+
 		InfoLog("Read SQL from `%s` and run query...", sqlfile)
 		InfoLog("Query: `%s`", query)
-		
-		// SQL実行
+
 		result, err := db.Exec(query)
 		if err != nil {
 			return err
 		}
-		// 影響行数取得
+
 		rowsAffected, _ := result.RowsAffected()
 		InfoLog("%d rows affected", rowsAffected)
 		InfoLog("Batch finished")
-		
+
 		return nil
 	}
 
@@ -72,12 +68,12 @@ func run(args []string) error {
 func InfoLog(message string, args ...interface{}) {
 	nowTime := time.Now()
 	const timefmt = "2006/01/02 15:04:05"
-	
+
 	msg := fmt.Sprintf(message, args...)
-	
+
 	fmt.Printf("[INFO] %s: %s\n", nowTime.Format(timefmt), msg)
 }
 
-func GetConnectionString() (string) {
+func GetConnectionString() string {
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/pakuyuya/note-docker
+
+go 1.12
+
+require (
+	github.com/lib/pq v1.2.0 // indirect
+	github.com/urfave/cli v1.21.0 // indirect
+)
